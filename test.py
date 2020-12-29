8473d8c001e633c0b4ad7e4a3a6b6c99271d1b1b
vcrl <vincentruelle.pro@gmail.com>
2020-12-29 18:14:48 +0100

    init

--- test.py
+++
@@ -1,5 +0,0 @@
-choice = "3"
-try:
-    int(choice)
-except ValueError:
-    print('ERROR !')
--- testcat.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://fr.openfoodfacts.org/categories.json"
-response = requests.get(url)
-response = response.json()
-
-for category in response['tags']:
-    if category['name'] == 'Snacks':
-        print(category['name'])
